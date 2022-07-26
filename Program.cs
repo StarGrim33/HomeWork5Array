9cc601f1309d3b6a62e992c591216afee3b1671d
StarGrim33 <prosky33@gmail.com>
2022-07-26 21:34:21 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace JuniorHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[30];
+            Random random = new Random();
+            int minValue = 1;
+            int maxValue = 10;
+            int countOfRepeat = 0;
+            int startCountKey = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minValue, maxValue);
+                Console.Write(numbers[i] + ",");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length -1; j++)
+                {
+                    if (numbers[j + 1] > numbers[j])
+                    {
+                        int sort = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = sort;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] == numbers[i+1])
+                {
+                    startCountKey++;
+                    Console.Write(numbers[i]);
+                }
+            }
+        }
+    }
+}
